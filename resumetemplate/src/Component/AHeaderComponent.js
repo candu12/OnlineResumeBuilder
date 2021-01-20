37d8d0016bbb7551e90c30d897ee61b7697006ac
Chandu S <chandurebels148@gmail.com>
2021-01-20 19:57:40 +0530

    Semicolon missing

--- resumetemplate/src/Component/AHeaderComponent.js
+++ resumetemplate/src/Component/AHeaderComponent.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react'
-import {BsPerson} from "react-icons/bs";
-import {MdLockOpen} from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
+import { MdLockOpen } from "react-icons/md"
 class HeaderComponent extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-                 
+
         }
     }
-    
+
 
     render() {
-        return ( 
+        return (
             <div>
-                
+
                 <nav class=" bg-dark">
-        
+
                     <a class="active" href="/admin" className="navbar-brand   text-light p-2 "><h3 >WELCOME ADMIN</h3></a>
-                    <a href="/login"  className="text-light  text-light "><BsPerson />LOGIN</a>
-                    <a href="/forgetpassword" className="text-light p-5"><MdLockOpen/>FORGETPASSWORD</a>
-                    <a href="/"className="   text-light">HOME</a>
-                  
-                   </nav>
-                   <br></br>
-                  
-           <img src="https://media0.giphy.com/media/gh0RRgkTXedvF0pDc0/giphy.gif?cid=ecf05e4704e793f2189e7657330a99727f3fd7abc923e942&rid=giphy.gif" width="1400" height="620" className="float-right"/>       
+                    <a href="/login" className="text-light  text-light "><BsPerson />LOGIN</a>
+                    <a href="/forgetpassword" className="text-light p-5"><MdLockOpen />FORGETPASSWORD</a>
+                    <a href="/" className="   text-light">HOME</a>
+
+                </nav>
+                <br></br>
+
+                <img src="https://media0.giphy.com/media/gh0RRgkTXedvF0pDc0/giphy.gif?cid=ecf05e4704e793f2189e7657330a99727f3fd7abc923e942&rid=giphy.gif" width="1400" height="620" className="float-right" />
             </div>
-            
-)
+
+        )
     }
 }
 
